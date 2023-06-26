9b8aa91d7955e4f0d7edba8320630770b97109d8
Aniede Chibundu David <ndubest56@gmail.com>
2023-06-26 18:33:52 +0100

    handling props, states and events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import {useEffect} from 'react';
+import './App.css';
+import {useState, useEffect} from 'react';
 
-//e213675b
-const Api_url = 'http://www.omdbapi.com/?i=tt3896198&apikey=e213675b'
+const Person = (props) => {
+  return (
+    <>
+      <h1> Name: {props.name}</h1>
+      <h3>Level: {props.level}</h3>
+      <p>Age: {props.age}</p>
+      <p>Marital Status: {props.status}</p>
+    </>
+  )
+}
 
-const App = () => {
-   return (
-    <h1><center>Hello</center></h1>
-   );
+const CounterButton = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You have changed the code to" +counter)
+  }, [counter])
+  
+  return (
+    <>
+      <button onClick={() => {setCounter((next) => next + 1)}}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => {setCounter( (prev) => prev - 1)}} >-</button>
+    </>
+  )
+  
+}
+
+
+const App = () => {  
+  const name = "Chibundu";
+  return (
+  <>
+    <div className='App'>
+     <CounterButton/>
+          
+      <Person name="Gifty Love" level='Graduate' age={26} status='Married'/>
+      <Person name={"Chibundu"} level={"Graduate"} age={32} status={"Maried"} />
+      <Person name={"Akuoma"} level={"Nursery"} age={2} status={"Baby"} />
+    </div>
+
+    <div className="App">
+      <h1>Hello {name}, </h1>
+      {name ? (<>{name} is a good name</>) : (<h2>You have not given your name</h2>)}      
+    </div>
+    </>
+  );
+  
 }
 
 export default App;
