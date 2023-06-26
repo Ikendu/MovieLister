9cd1288d63cc091237006f11c5dc385f2984fe8e
Aniede Chibundu David <ndubest56@gmail.com>
2023-06-26 18:12:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//e213675b
+const Api_url = 'http://www.omdbapi.com/?i=tt3896198&apikey=e213675b'
+
+const App = () => {
+   return (
+    <h1><center>Hello</center></h1>
+   );
 }
 
 export default App;
