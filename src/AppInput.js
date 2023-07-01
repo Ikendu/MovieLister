c315fe4c21f06e41f9a2e0356c6fd4739138a134
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-01 10:35:42 +0100

    render input onSubmit

--- src/AppInput.js
+++ src/AppInput.js
@@ -4,22 +4,33 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            input: ''
+            input: '', submit: ''
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event){
         this.setState({
             input: event.target.value
         })
     }
+    handleSubmit(event){
+        this.setState({
+            submit: this.state.input
+        })
+        event.preventDefault()
+    }
 
     render(){
         return(
             <div>
+                <form onSubmit={this.handleSubmit} >
                 <input onChange={this.handleChange} value={this.state.input} />
+                <input type='submit'/>
+                </form>
                 <h2>User input is displayed below</h2>
                 <h3>{this.state.input}</h3>
+                <h1>{this.state.submit}</h1>
             </div>
         )
     }
