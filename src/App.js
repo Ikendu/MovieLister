6b26dc7108f563b0efa59cdf890fded81d58261d
Aniede Chibundu <ndubest56@gmail.com>
2024-03-29 10:59:05 +0100

    search with the enter buttton

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import MovieCard from './MovieCard';
-import SearchIcon from './search.svg';
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
+import SearchIcon from "./contents/search.svg";
+import axios from "axios";
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e213675b'
-
-
+const API_URL = "http://www.omdbapi.com/?apikey=e213675b";
 
 const App = () => {
-    const [movies, setMovies] = useState([])
-    const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+  const searchMovies = async (title) => {
+    // const response = await fetch(`${API_URL}&s=${title}`);
+    // const data = await response.json();
+    const { data } = await axios.get(`${API_URL}&s=${title}`);
+    console.log(`Data`, data);
 
-        setMovies(data.Search);
-        console.log(data.Search);
+    setMovies(data?.Search);
+    console.log(data);
+  };
+
+  const handleEnterKey = async (title, e) => {
+    // e.preventDefault();
+    if (e.key === `Enter`) {
+      const { data } = await axios.get(`${API_URL}&s=${title}`);
+      console.log(`Enter key data`, data);
     }
+  };
 
-    useEffect(() => {
-        searchMovies('Nigeria');
-    }, []);
+  useEffect(() => {
+    searchMovies("Nigeria");
+  }, []);
 
-    return (
-        <div className="app">
-            <h1>MovieLife</h1>
+  return (
+    <div className="app">
+      <h1>MovieLife</h1>
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => handleEnterKey(searchTerm, e)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-           
-
-            <div className="search">
-                <input placeholder='Search for Movies' 
-                value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} 
-                />
-                <img src={SearchIcon} 
-                alt='search' onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-             {
-                movies?.length > 0 ?
-                ( 
-                   <div className='container'>
-                     {movies.map((movie) => (
-                        <MovieCard movie={movie} />
-                     ))}
-                   </div> 
-                 ) : (
-                    <div className='Empty'>
-                        <h2>No Movie Found</h2>
-                    </div>
-                 )
-            }
-            
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-    )
-}
+      ) : (
+        <div className="Empty">
+          <h2>No Movie Found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 export default App;
