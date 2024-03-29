4b89918c58b0e45f6aff2aed0c73b2cf4aa40831
Aniede Chibundu <ndubest56@gmail.com>
2024-03-29 13:21:09 +0100

    having both the both onClick and keyDown on one function

--- src/App.js
+++ src/App.js
@@ -10,23 +10,29 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const searchMovies = async (title) => {
+  const searchMovies = async (title, e) => {
     // const response = await fetch(`${API_URL}&s=${title}`);
     // const data = await response.json();
-    const { data } = await axios.get(`${API_URL}&s=${title}`);
-    console.log(`Data`, data);
 
-    setMovies(data?.Search);
-    console.log(data);
-  };
-
-  const handleEnterKey = async (title, e) => {
-    // e.preventDefault();
-    if (e.key === `Enter`) {
+    if (e?.key === `Enter`) {
       const { data } = await axios.get(`${API_URL}&s=${title}`);
-      console.log(`Enter key data`, data);
+      console.log(`Data from Enter key`, data);
+      setMovies(data?.Search);
+    } else if (e?.button === 0 || e?.button === 1) {
+      const { data } = await axios.get(`${API_URL}&s=${title}`);
+      console.log(`Data`, data);
+      setMovies(data?.Search);
     }
   };
+
+  //submitting with Enter Key
+  //   const handleEnterKey = async (title, e) => {
+  //     // e.preventDefault();
+  //     if (e.key === `Enter`) {
+  //       const { data } = await axios.get(`${API_URL}&s=${title}`);
+  //       console.log(`Enter key data`, data);
+  //     }
+  //   };
 
   useEffect(() => {
     searchMovies("Nigeria");
@@ -40,12 +46,12 @@ const App = () => {
           placeholder="Search for Movies"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyDown={(e) => handleEnterKey(searchTerm, e)}
+          onKeyDown={(e) => searchMovies(searchTerm, e)}
         />
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => searchMovies(searchTerm)}
+          onClick={(e) => searchMovies(searchTerm, e)}
         />
       </div>
 
